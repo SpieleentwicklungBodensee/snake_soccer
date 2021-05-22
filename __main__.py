74a28bf550a8911c9e1c4d26170edad21df01278
kikomedia <45310068+kikomedia@users.noreply.github.com>
2021-05-22 13:03:23 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -51,7 +51,7 @@ def toggleFullscreen():
     else:
         window = pygame.display.set_mode((WIN_W, WIN_H), 0)
 
-
+# base entity
 class GameObject():
     def __init__(self, x, y):
         self.x = x
