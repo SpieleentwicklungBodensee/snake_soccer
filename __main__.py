658d853514d02445a12ab4056c3236299ec2101e
xrrawva <67735592+xrrawva@users.noreply.github.com>
2021-05-22 17:42:40 +0200

    Revert "Merge pull request #15 from SpieleentwicklungBodensee/xrrawva"
    This reverts commit 0f562dc802c3ae4e2b00af64e6489fa021600c53, reversing
    changes made to 96760277751c048f76c3b7fccc2925f63eb17fc4.

--- __main__.py
+++ __main__.py
@@ -9,15 +9,20 @@ from globalconst import *
 from gameobjects import *
 from bitmapfont import BitmapFont
 from worm import Worm
-from ball import Ball
+from playerobject import *
 
 import network
+
+playerId = 0    # 0 = worm, other id = player
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--connect')
 parser.add_argument('--port', type=int, default=2000)
 parser.add_argument('--host', action='store_true')
+parser.add_argument('--id', type=int, default=0)    # only for testing
 args = parser.parse_args()
+
+playerId = args.id  # only for testing
 
 net = None
 if args.connect is not None:
@@ -28,8 +33,6 @@ elif args.host:
 
 
 pygame.display.init()
-
-player = GameObject(0,0)
 
 if FULLSCREEN:
     window = pygame.display.set_mode(pygame.display.list_modes()[0], pygame.FULLSCREEN)
@@ -78,11 +81,19 @@ level = ['########################################',
 tiles = {'#': pygame.image.load('gfx/wall.png'),
          'H': pygame.image.load("gfx/worm_head.png"),
          'B': pygame.image.load("gfx/worm_body.png"),
+         '1': pygame.image.load('gfx/player1.png'),
+         '2': pygame.image.load('gfx/player2.png'),
+         '3': pygame.image.load('gfx/player3.png')
          }
 
 
+
 worm   = Worm(math.floor(len(level[0])/2),math.floor(len(level)/2),TILE_W,TILE_H)
-ball   = Ball(SCR_W/4,SCR_H/2,TILE_W,TILE_H)
+player = Player(4, 4, '1')
+
+players = [worm, player]
+ownPlayer = players[playerId]
+
 
 def toggleFullscreen():
     global FULLSCREEN, window
@@ -102,17 +113,13 @@ def controls():
                 return False
 
             if e.key == pygame.K_LEFT:
-                player.moveLeft()
-                worm.moveLeft()
+                ownPlayer.moveLeft()
             if e.key == pygame.K_RIGHT:
-                player.moveRight()
-                worm.moveRight()
+                ownPlayer.moveRight()
             if e.key == pygame.K_UP:
-                player.moveUp()
-                worm.moveUp()
+                ownPlayer.moveUp()
             if e.key == pygame.K_DOWN:
-                player.moveDown()
-                worm.moveDown()
+                ownPlayer.moveDown()
 
             if e.key == pygame.K_RETURN:
                 mods = pygame.key.get_mods()
@@ -121,17 +128,13 @@ def controls():
 
         if e.type == pygame.KEYUP:
             if e.key == pygame.K_LEFT:
-                player.stopLeft()
-                worm.stopLeft()
+                ownPlayer.stopLeft()
             if e.key == pygame.K_RIGHT:
-                player.stopRight()
-                worm.stopRight()
+                ownPlayer.stopRight()
             if e.key == pygame.K_UP:
-                player.stopUp()
-                worm.stopUp()
+                ownPlayer.stopUp()
             if e.key == pygame.K_DOWN:
-                player.stopDown()
-                worm.stopDown()
+                ownPlayer.stopDown()
 
             if e.key == pygame.K_F11:
                 global FPS
@@ -147,35 +150,31 @@ def controls():
         if e.type == pygame.JOYAXISMOTION:
             if e.axis == 0:
                 if e.value < -JOY_DEADZONE:
-                    player.moveLeft()
+                    ownPlayer.moveLeft()
                 elif e.value > JOY_DEADZONE:
-                    player.moveRight()
+                    ownPlayer.moveRight()
                 else:
-                    if player.xdir < 0:
-                        player.stopLeft()
-                    if player.xdir > 0:
-                        player.stopRight()
+                    if ownPlayer.xdir < 0:
+                        ownPlayer.stopLeft()
+                    if ownPlayer.xdir > 0:
+                        ownPlayer.stopRight()
 
             if e.axis == 1:
                 if e.value < -JOY_DEADZONE:
-                    player.moveUp()
+                    ownPlayer.moveUp()
                 elif e.value > JOY_DEADZONE:
-                    player.moveDown()
+                    ownPlayer.moveDown()
                 else:
-                    if player.ydir < 0:
-                        player.stopUp()
-                    if player.ydir > 0:
-                        player.stopDown()
+                    if ownPlayer.ydir < 0:
+                        ownPlayer.stopUp()
+                    if ownPlayer.ydir > 0:
+                        ownPlayer.stopDown()
 
         if e.type == pygame.JOYBUTTONDOWN:
-            if e.button == 1:
-                player.doJump()
-            elif e.button == 0:
-                player.interact()
+            pass
 
         if e.type == pygame.JOYBUTTONUP:
-            if e.button == 1:
-                player.cancelJump()
+            pass
 
     return True
 
@@ -189,41 +188,44 @@ def render():
             if level[y][x] == '#':
                 screen.blit(tiles['#'], (x * TILE_W, y * TILE_H))
 
-    # render worm
-    worm.draw(screen,tiles)
-
-    # render ball
-    ball.draw(screen,tiles)
+    # render players
+    for p in players:
+        p.draw(screen, tiles)
 
 def update():
-    worm.update()
-    ball.update()
-    pass
+    global players
+    for p in players:
+        p.update()
 
-
+    if net is not None:
+        players = net.update(players)
+        ownPlayer = players[playerId]
 
 
 tick = 0
 running = True
 
-while running:
-    tick += 1
+try:
+    while running:
+        tick += 1
 
-    render()
+        render()
 
-    pygame.transform.scale(screen, window.get_size(), window)
-    pygame.display.flip()
+        pygame.transform.scale(screen, window.get_size(), window)
+        pygame.display.flip()
 
-    cont = controls()
+        cont = controls()
 
-    if not cont:
-        running = False
+        if not cont:
+            running = False
 
-    update()
-
-
-    clock.tick(FPS)
+        update()
 
 
-if net is not None:
-    net.stop()
+        clock.tick(FPS)
+
+finally:
+    if net is not None:
+        net.stop()
+
+    pygame.quit()
